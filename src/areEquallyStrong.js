f9d69e12a034029a797f90f5d83dde18686fcd30
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 15:53:12 -0300

    update areEquallyStrong.js

--- src/areEquallyStrong.js
+++ src/areEquallyStrong.js
@@ -8,3 +8,20 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test areEquallyStrong
+
+// alternative solution
+// function solution(yourLeft, yourRight, friendsLeft, friendsRight) {
+//     return yourLeft + yourRight === friendsLeft + friendsRight && (yourLeft === friendsLeft || yourLeft === friendsRight);
+// }
+
+// alternative solution
+// function solution(yourLeft, yourRight, friendsLeft, friendsRight) {
+//     var me = [yourLeft, yourRight].sort().join('');
+//     var friend = [friendsLeft, friendsRight].sort().join('');
+//     return me === friend;
+// }
+
+// alternative solution
+// function solution(yourLeft, yourRight, friendsLeft, friendsRight) {
+//     return Math.max(yourLeft, yourRight) === Math.max(friendsLeft, friendsRight) && Math.min(yourLeft, yourRight) === Math.min(friendsLeft, friendsRight);
+// }
